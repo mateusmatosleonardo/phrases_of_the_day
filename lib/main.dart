d4aa611c51d8eb452d511776095e2933bacb414c
Mateus Leonardo <m4teusmatos@gmail.com>
2022-02-01 20:47:38 -0300

    adding elevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,15 @@ void main() {
       color: Colors.white,
       child: Column(
         children: const [
-          Text(
-            "Lorem ipsum",
-            style: TextStyle(
-                decoration: TextDecoration.underline,
-                decorationColor: Colors.black,
-                decorationStyle: TextDecorationStyle.solid,
-                letterSpacing: 0,
-                wordSpacing: 0,
-                fontSize: 35,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.normal,
-                color: Colors.black),
-          )
+          ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Pressione aqui",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black),
+              ))
         ],
       ),
     ),
