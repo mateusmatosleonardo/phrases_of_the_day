a2513cfd9083b539dd4a55efa06d11fab03290a2
Mateus Leonardo <m4teusmatos@gmail.com>
2022-02-01 08:47:50 -0300

    manipuling text

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ void main() {
   // função para executar o app
   runApp(MaterialApp(
     title: "Frases do dia",
-    // home: estrutura inicial do App
-    home: Row(
-      children: const [Text("T1"), Text("T2"), Text("T3")],
-      // color: Colors.white,
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: const [
+          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        ],
+      ),
     ),
+    // home: estrutura inicial do App
   ));
 }
