f02e990c904e3ba7635da4244385bebd57d4bfb3
Mateus Leonardo <m4teusmatos@gmail.com>
2022-02-06 12:48:20 -0300

    adding button

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,35 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Frases do dia"),
+        title: const Text("Frases do dia"),
         backgroundColor: Colors.green,
       ),
       body: Container(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         decoration:
             BoxDecoration(border: Border.all(width: 3, color: Colors.amber)),
         child: Column(
-          children: [Image.asset("images/logo.png")],
+          children: [
+            Image.asset("images/logo.png"),
+            const Text(
+              "Clique abaixo para gerar uma frase",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                  fontSize: 17,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black),
+            ),
+            ElevatedButton(
+              child: const Text(
+                "Nova frase",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+            )
+          ],
         ),
       ),
     );
